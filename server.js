a5045e1dfed97a1b2e4a9c77f66efd2069ef5a72
JuanAndresMendieta <ingejuanandresmendieta@gmail.com>
2026-06-26 13:36:33 -0500

    Cambio prueba funcionamiento

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ function sendWhatsApp(phone, { mediaId, texto }) {
       components: [
         {
           type      : 'header',
-          parameters: [{ type: 'image', image: { id: parseInt(mediaId) } }],
+          parameters: [{ type: 'image', image: { id: String(mediaId) } }],
         },
         {
           type      : 'body',
